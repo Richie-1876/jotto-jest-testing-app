e293710425a198bd07ae2bef7b87322602e6a78c
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-08-26 13:47:15 +0100

    setup redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function - successReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action to be reduced
+ * @returns {boolean} - new sucess status
+ */
+
+export default (state, action) => {
+  return null;
+};
