ad12dfcfb0996d0aafb4679cde99e07462cb9130
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-08-26 14:54:52 +0100

    Set up Input component and storeFactory test util

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,26 @@
 import checkPropTypes from "check-prop-types";
-/**
+import { createStore } from "redux";
+
+import rootReducer from "../src/reducers";
+
+/***************************************************************************
+ *Create a testing store with imported reducers, middleware and initialState
+ * globals: rootReducer
+ * @param {object} initialState - initial state for the store
+ * @function storeFactory
+ * @returns {Store} - Redux store
+ ****************************************************************************/
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
+
+/****************************************************************************
  * returns node(s) with given data-test attribute
  * @param - {ShallowWrapper} wrapper - Enzyme shallow wrapper
  * @param - {string} val - value of the data-test attribute for search
  * @returns {ShallowWrapper}
- */
+ ****************************************************************************/
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
