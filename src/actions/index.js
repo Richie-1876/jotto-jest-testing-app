77dc6f98ccb027187e26db6cea8fe1bedaed4a8b
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-08-26 17:38:39 +0100

    Add secretWord action creater and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import { getLetterMatchCount } from "../helpers";
+import axios from "axios";
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
   GUESS_WORD: "GUESS_WORD",
+  SET_SECRET_WORD: "SET_SECRET_WORD",
 };
 
 /****************************************************************************
@@ -25,5 +27,16 @@ export const guessWord = (guessedWord) => {
         type: actionTypes.CORRECT_GUESS,
       });
     }
+  };
+};
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    return axios.get("http://localhost:3030").then((response) => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: response.data,
+      });
+    });
   };
 };
